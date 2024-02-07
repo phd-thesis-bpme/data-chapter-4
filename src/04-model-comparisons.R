f501c32b36c67739f28594e0be0d75197b9006f4
Brandon Edwards <brandon.edwards144@gmail.com>
2024-02-07 15:21:50 -0500

    setting up code for eventual detectability model results

--- src/04-model-comparisons.R
+++ src/04-model-comparisons.R
@@ -14,6 +14,7 @@ library(ggpubr)
 
 route <- readRDS("data/generated/model_runs/Ovenbird-route.rds")
 point <- readRDS("data/generated/model_runs/Ovenbird-point.rds")
+detectability <- readRDS("data/generated/model_runs/Ovenbird-detectability.rds")
 
 ####### Indices and Trends ########################
 
@@ -27,16 +28,25 @@ trends_point <- generate_trends(indices = indices_point)
 (trend_map_point <- plot_map(trends_point))
 indices_plot_point <- plot_indices(indices = indices_point)
 
+indices_detectability <- generate_indices(model_output = detectability)
+trends_detectability <- generate_trends(indices = indices_detectability)
+(trend_map_detectability <- plot_map(trends_detectability))
+indices_plot_detectability <- plot_indices(indices = indices_detectability)
+
+# Generate a new list of ggarranged trajectory plots comparing all three
+
+
 ####### Output ####################################
 
 png(filename = "output/plots/oven-map.png",
     width = 10, height = 6, units = "in", res = 300)
-ggarrange(trend_map_route, trend_map_point, nrow = 1,
-          labels = c("Route", "Point"))
+ggarrange(trend_map_route, trend_map_point, trend_map_detectability, nrow = 1,
+          labels = c("Route", "Point", "Detectability"))
 dev.off()
 
 png(filename = "output/plots/oven-trajectory.png",
     width = 10, height = 6, units = "in", res = 300)
-ggarrange(indices_plot_route$continent, indices_plot_point$continent, nrow = 1,
-          labels = c("Route", "Point"))
+ggarrange(indices_plot_route$continent, indices_plot_point$continent, 
+          indices_plot_detectability$continent, nrow = 1,
+          labels = c("Route", "Point", "Detectability"))
 dev.off()
