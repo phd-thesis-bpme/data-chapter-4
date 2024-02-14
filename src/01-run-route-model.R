2a02ad2216d5583dd377c19a71aa3707a1313904
Brandon Edwards <brandon.edwards144@gmail.com>
2024-02-14 11:17:31 -0500

    Running more species at the route level

--- src/01-run-route-model.R
+++ src/01-run-route-model.R
@@ -3,7 +3,7 @@
 # BBS Point Level
 # <01-run-route-model.R>
 # Created July 2023
-# Last Updated December 2023
+# Last Updated February 2024
 
 ####### Import Libraries and External Files #######
 
@@ -11,26 +11,37 @@ library(bbsBayes2)
 
 ####### Set Constants #############################
 
-sp <- "Ovenbird"
+species_list <- c("Ovenbird", "Swainson's Thrush", "American Crow",
+        "Blue-headed Vireo", "Eastern Phoebe", "Tennessee Warbler",
+        "Yellow Warbler")
+
 st <- "latlong"
 
 ####### Read Data #################################
 
 ####### Main Code #################################
 
-bbs_stratified <- stratify(by = st, species = sp)
+for (sp in species_list)
+{
+  print(sp)
+  bbs_stratified <- stratify(by = st, species = sp)
+  
+  # Limit analysis to only Ontario, Canada
+  bbs_stratified$routes_strata <- 
+    bbs_stratified$routes_strata[which(bbs_stratified$routes_strata$st_abrev == "ON"), ]
+  
+  mod_prepped <- prepare_data(strata_data = bbs_stratified,
+                              min_year = 2000,
+                              min_n_routes = 1) %>%
+    prepare_spatial(strata_map = load_map(st)) %>%
+    prepare_model(model = "gamye", model_variant = "spatial")
+  
+  model_run <- run_model(model_data = mod_prepped,
+                         output_basename = paste0(sp, "-route"),
+                         output_dir = "data/generated/model_runs",
+                         overwrite = TRUE)
+  
+  gc()
+}
 
-# Limit analysis to only Ontario, Canada
-bbs_stratified$routes_strata <- 
-  bbs_stratified$routes_strata[which(bbs_stratified$routes_strata$st_abrev == "ON"), ]
 
-mod_prepped <- prepare_data(strata_data = bbs_stratified,
-                            min_year = 2000,
-                            min_n_routes = 1) %>%
-  prepare_spatial(strata_map = load_map(st)) %>%
-  prepare_model(model = "gamye", model_variant = "spatial")
-
-model_run <- run_model(model_data = mod_prepped,
-                       output_basename = paste0(sp, "-route"),
-                       output_dir = "data/generated/model_runs",
-                       overwrite = TRUE)
