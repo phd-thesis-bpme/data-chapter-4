b98445fc50eccdb15b7c0f604e4a77cca7a4360a
Brandon Edwards <brandon.edwards144@gmail.com>
2024-03-15 10:53:13 -0400

    neat figure idea, to be exapnded upon later

---
+++ src/99-neat-figure.R
@@ -0,0 +1,3 @@
+#' Two panel GIF that shows on the left, a map for one BBS route (so 50 poiints)
+#' which changing landcover over time, and on the Right a plot that shows the change
+#' in expected detectability for a given species
