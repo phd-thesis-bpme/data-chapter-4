8bf1a529e587365e23286b276e26da84a1176b63
Brandon Edwards <brandon.edwards144@gmail.com>
2024-03-23 13:05:41 -0400

    updating nalcms extraction code for use with all points, not just NA points

--- src/00c-extract-nalcms-landcover.R
+++ src/00c-extract-nalcms-landcover.R
@@ -11,23 +11,21 @@ library(terra)
 
 ####### Read Data #################################
 
-bbs_sites <- readRDS(file = "data/generated/bbs_sites_aci.RDS"); bbs_sites$rt_st <- NULL
+bbs_sites <- readRDS(file = "data/generated/bbs_sites.RDS"); bbs_sites$rt_st <- NULL
 nalcms_2010 <- rast("data/raw/spatial/nalcms/CAN_Land_cover_2010v2_30m_TIF/CAN_NALCMS_landcover_2010v2_30m/data/CAN_NALCMS_landcover_2010v2_30m.tif")
 nalcms_2015 <- rast("data/raw/spatial/nalcms/can_land_cover_2015v3_30m_tif/CAN_NALCMS_landcover_2015v3_30m/data/CAN_NALCMS_landcover_2015v3_30m.tif")
 nalcms_2020 <- rast("data/raw/spatial/nalcms/can_land_cover_2020_30m_tif/CAN_NALCMS_landcover_2020_30m/data/CAN_NALCMS_landcover_2020_30m.tif")
 
 ####### Main Code #################################
 
-#' We need to go back and check which Ontario (with year > 2011) did NOT receive
-#' landcover values. For those ones, we need to use the NALCMS data and interpolate.
+bbs_sites$forest_coverage <- NA
 
 # First, get the indices that didn't work
-na_indices<- which(bbs_sites$year >= 2011 &
+indices<- which(bbs_sites$year >= 2011 &
                      bbs_sites$st_abrev == "ON" &
-                     bbs_sites$latitude < 1000 &
-                     is.na(bbs_sites$forest_coverage))
+                     bbs_sites$latitude < 1000)
 
-bbs_reduced <- bbs_sites[na_indices, ]
+bbs_reduced <- bbs_sites[indices, ]
 
 routes <- unique(bbs_reduced$route)
 
@@ -125,7 +123,7 @@ for (r in routes)
 #' Easiest way is to probably remove all the ones that previously had NA values, then add this
 #' new DF back in. Saves possible merging issues.
 
-bbs_sites_red <- bbs_sites[-na_indices, ]
+bbs_sites_red <- bbs_sites[-indices, ]
 bbs_sites_fc <- rbind(bbs_sites_red, bbs_reduced)
 bbs_sites <- bbs_sites_fc
 
