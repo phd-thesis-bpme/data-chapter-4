a3b61a5e3274ca33ea9dd2d9949dcb3bbc555f45
Brandon Edwards <brandon.edwards144@gmail.com>
2024-05-10 10:46:41 -0400

    WIP script to extract varprop parameters from fitted models and plot distribution

---
+++ src/08-extract-varprop-parameters.R
@@ -0,0 +1,36 @@
+####### Script Information ########################
+# Brandon P.M. Edwards
+# BBS Point Level
+# 08-extract-varprop-parameters.R
+# Created May 2024
+# Last Updated May 2024
+
+####### Import Libraries and External Files #######
+
+library(cmdstanr)
+library(bayesplot)
+library(ggpubr)
+theme_set(theme_pubclean())
+bayesplot::color_scheme_set("red")
+
+####### Set Constants #############################
+
+sp <- "OVEN"
+
+####### Read Data #################################
+
+detectability_model <- readRDS(paste0("output/model_runs/", sp, "-detectability.rds"))
+
+####### Main Code #################################
+
+avail_plot <- bayesplot::mcmc_areas(detectability_model$model_fit$draws("zeta")) 
+
+percept_plot <- bayesplot::mcmc_areas(detectability_model$model_fit$draws("xi")) +
+  scale_y_discrete(labels = c("Intercept","Forest","Road","RoadForest"))
+
+####### Output ####################################
+
+png(filename = paste0("output/plots/", sp, "-varprop-percept.png"),
+    width = 6, height = 4, res = 300, units = "in")
+print(percept_plot)
+dev.off()
